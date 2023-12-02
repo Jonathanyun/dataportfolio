069f8c9b7e917377bf02468a111b1cb89c04c5fc
Jonathan Yun <sukwonyun@gmail.com>
2023-12-01 18:35:11 -0800

    Updates to ReadMe and fix for duplicated columns

--- README.txt
+++ README.txt
@@ -1,1 +1,14 @@
-Data Portfolio for Jonathan Yun
+# Data Portfolio for Jonathan Yun
+___
+
+### About Me:
+
+I am an aspiring data scientist from California. 
+
+- I am currently unemployed but actively looking for opportunities in the tech space. 
+
+___
+
+### Project List: 
+
+Titanic - 
--- titanic/src/manipulation.py
+++ titanic/src/manipulation.py
@@ -9,15 +9,13 @@ def titanic_manipulation(df):
     """ Function that manipulates the titanic data set"""
 
     if isinstance(df, pd.DataFrame):
-        # Dropping the name columns
-        df.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
-
         # Start of Feature Engineering dataset variables
         one_hot_encoded_data = pd.get_dummies(df, columns = ['Pclass', 'Sex', 'Embarked'])
         df_encoded = pd.concat([df, one_hot_encoded_data], axis = 1)
-        df_encoded = df_encoded.drop(['Pclass', 'Sex', 'Embarked', 'Name', 'Ticket', 'Cabin'], axis = 1)
-
-        return df_encoded
+        df_encoded = df_encoded.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
+        df_encoded = df_encoded.loc[:,~df_encoded.columns.duplicated()].copy()
+        
+        return pd.DataFrame(df_encoded)
     else:
         return "Object is not a Pandas DataFrame"
 
